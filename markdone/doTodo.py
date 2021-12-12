1e7fbd2fb53cee56a1effa095405d2aa281497d2
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2021-12-12 20:55:23 +0200

    5.02

--- markdone/doTodo.py
+++ markdone/doTodo.py
@@ -13,19 +13,23 @@ logging.basicConfig(
 def main():
 
     url = f"http://{os.environ['API_URL']}/todos"
-    r = requests.get(url, timeout=10)
+    r = requests.get(url, timeout=60)
     try:
         todos = r.json()
         count_of_todos = len(todos)
         if count_of_todos > 0:
             random_todo = todos[random.randint(0, count_of_todos-1)]
-            url = f"{url}/{random_todo['ID']}"
-            try:
-                r = requests.put(url, timeout=10)
-                if r.status_code == 200:
-                    logger.info(f"successfully marked {todos[random_todo]} done")
-            except Exception as error:
-                logger(f"put request failed {url}, error: {error}")
+            logger.info(f"found {random_todo} task")
+            if "id" in random_todo.keys():
+                url = f"{url}/{random_todo['id']}"
+                try:
+                    r = requests.put(url, timeout=60)
+                    if r.status_code == 200:
+                        logger.info(f"successfully marked {random_todo} done")
+                except Exception as error:
+                    logger.info(f"put request failed {url}, error: {error}")
+            else:
+                logger.info("no key id in task")
         else:
             logger.info("no todos to do")
     except Exception as error:
